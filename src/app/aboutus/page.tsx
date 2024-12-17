922551e1e02dd3dd45b19c2c3f481a80413fe36a
JSALDOCONSTRUUCTION <jsaldoconstruction4@gmail.com>
2024-12-17 21:59:14 +0530

    header

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -1,10 +1,15 @@
 import ComingSoon from '@/components/ComingSoon';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
 import React from 'react';
 
 const page = () => {
   return (
     <>
+    <Header />
       <ComingSoon />
+      <Footer />
+
     </>
   );
 };

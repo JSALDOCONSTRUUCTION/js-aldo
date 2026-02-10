a126bf9d543fdd0a6038fa2c92bc5047da97cc39
JSALDOCONSTRUUCTION <jsaldoconstruction4@gmail.com>
2026-02-10 17:40:44 +0530

    test

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -41,7 +41,7 @@ const Footer: React.FC = () => {
                 </li>
                 <li>
                   <a href="/window" className="text-white hover:text-gray-300">
-                    Windows
+                    Windowsd
                   </a>
                 </li>
                 <li>
